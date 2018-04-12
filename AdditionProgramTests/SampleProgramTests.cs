850f991bbc7836265fd45802908feb4ffa74c498
Manohari, Gowri (Cognizant) <670312@cognizant.com>
2018-04-12 16:35:46 +0530

    Add project files.

---
+++ AdditionProgramTests/SampleProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AdditionProgram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionProgram.Tests
+{
+    [TestClass()]
+    public class SampleProgramTests
+    {
+        [TestMethod()]
+        public void sumTest()
+        {
+            SampleProgram sp = new SampleProgram();
+
+            Assert.AreEqual(4, sp.sum(2, 2));
+
+        }
+    }
+}
